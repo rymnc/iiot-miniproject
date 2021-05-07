db507aa43b074709dce8608b05fd9b4deeca5053
rymnc <aaryamannchallani7@gmail.com>
2021-05-07 10:41:21 +0530

    feat: api scaffold

---
+++ processes/api/src/plugins/support.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin'
+
+export interface SupportPluginOptions {}
+
+export default fp<SupportPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('someSupport', function () {
+    return 'hugs'
+  })
+})
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    someSupport(): string;
+  }
+}
