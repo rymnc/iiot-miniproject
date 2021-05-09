77ba2323f4b580a3f3d55c244c517fee1caec881
rymnc <aaryamannchallani7@gmail.com>
2021-05-09 10:29:18 +0530

    style: lint

--- processes/api/src/app.ts
+++ processes/api/src/app.ts
@@ -1,7 +1,7 @@
 import { join } from "path";
 import AutoLoad, { AutoloadPluginOptions } from "fastify-autoload";
 import { FastifyPluginAsync } from "fastify";
-import {} from 'fastify-cors'
+import {} from "fastify-cors";
 
 export type AppOptions = {} & Partial<AutoloadPluginOptions>;
 
@@ -9,13 +9,16 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts
 ): Promise<void> => {
-  void fastify.register(require('fastify-cors'), { 
+  void fastify.register(require("fastify-cors"), {
     // put your options here
-    origin: (origin: string, cb: (arg0: Error | null, arg1: boolean) => void) => {
-      cb(null, true)
-    }
-  })
-  
+    origin: (
+      origin: string,
+      cb: (arg0: Error | null, arg1: boolean) => void
+    ) => {
+      cb(null, true);
+    },
+  });
+
   void fastify.register(AutoLoad, {
     dir: join(__dirname, "plugins"),
     options: opts,
